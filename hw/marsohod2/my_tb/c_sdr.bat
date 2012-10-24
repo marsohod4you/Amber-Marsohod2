c7adfca1f554cef941968f066aea08937ad7d1e2
marsohod4you <nck.kovach@gmail.com>
2012-10-24 15:58:06 +0300

    delete unneccessary files related to simulations

--- hw/marsohod2/my_tb/c_sdr.bat
+++
@@ -1,1 +0,0 @@
-iverilog -o qqq -g2005 -DSDRAM=1 -DICARUS=1 -DAMBER_A23_CORE=1 -DNOMEMORY=1 -csrclist_sdr -I../../vlog/sdram/sdr_ctrl/trunk/rtl/core -I../../vlog/sdram/sdr_ctrl/trunk/rtl/wb2sdrc -I../../vlog/system/ -I../../vlog/amber23/ -I../../vlog/tb/
